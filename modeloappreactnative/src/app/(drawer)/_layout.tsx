ad93441ba873df3b2b8ebb2da1d0c027c230ed9a
Nathália Cericatto <nathalia.cericatto@gmail.com>
2026-06-03 19:37:17 -0300

    Ajustes

--- modeloappreactnative/src/app/(drawer)/_layout.tsx
+++ modeloappreactnative/src/app/(drawer)/_layout.tsx
@@ -1,7 +1,7 @@
+import CustomDrawerContent from '@/components/CustomDrawerContent';
 import { Ionicons } from '@expo/vector-icons';
 import { Drawer } from 'expo-router/drawer';
 import React from 'react';
-import CustomDrawerContent from '../../components/CustomDrawerContent';
 
 export default function DrawerLayout() {
   return (
@@ -27,6 +27,15 @@ export default function DrawerLayout() {
       />
 
       <Drawer.Screen
+        name="admin/index"
+        options={{
+          title: 'Painel Admin',
+          drawerIcon: ({ color, size }) =>
+            <Ionicons name="speedometer-outline" size={size} color={color} />,
+        }}
+      />
+
+      <Drawer.Screen
         name="admin/listar-filmes"
         options={{
           title: 'Filmes (Admin)',
@@ -37,6 +46,36 @@ export default function DrawerLayout() {
       <Drawer.Screen
         name="admin/cadastrar-filme"
         options={{ title: 'Cadastrar Filme', drawerItemStyle: { display: 'none' } }}
+      />
+      <Drawer.Screen
+        name="admin/editar-filme"
+        options={{ title: 'Editar Filme', drawerItemStyle: { display: 'none' } }}
+      />
+
+      <Drawer.Screen
+        name="admin/listar-generos"
+        options={{ title: 'Gêneros', drawerIcon: ({ color, size }) => <Ionicons name="pricetag-outline" size={size} color={color} /> }}
+      />
+      <Drawer.Screen
+        name="admin/cadastrar-genero"
+        options={{ title: 'Cadastrar Gênero', drawerItemStyle: { display: 'none' } }}
+      />
+      <Drawer.Screen
+        name="admin/editar-genero"
+        options={{ title: 'Editar Gênero', drawerItemStyle: { display: 'none' } }}
+      />
+
+      <Drawer.Screen
+        name="admin/listar-salas"
+        options={{ title: 'Salas', drawerIcon: ({ color, size }) => <Ionicons name="business-outline" size={size} color={color} /> }}
+      />
+      <Drawer.Screen
+        name="admin/cadastrar-sala"
+        options={{ title: 'Cadastrar Sala', drawerItemStyle: { display: 'none' } }}
+      />
+      <Drawer.Screen
+        name="admin/editar-sala"
+        options={{ title: 'Editar Sala', drawerItemStyle: { display: 'none' } }}
       />
 
       <Drawer.Screen
